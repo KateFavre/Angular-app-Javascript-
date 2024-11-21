ab658bfa723c7d57bef5403cfe0329bb1dd479a7
Kate Favre <katielfavre@gmail.com>
2024-11-20 16:43:51 -0800

    Part D

--- my-D280/src/app/app.routes.ts
+++ my-D280/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path: 'app', component:AppComponent},
+    { path: '', redirectTo: '/app.component', pathMatch: 'full' }, // redirecting default url 
+  ];
